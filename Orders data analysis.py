8ddc98591d8dc260afd7a9ed0e8e53a0d5c78dcc
amitsakhare <100063124+AMITTT7@users.noreply.github.com>
2024-04-26 12:50:42 +0530

    Update Orders data analysis.py

--- Orders data analysis.py
+++ Orders data analysis.py
@@ -1,119 +1,83 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[76]:
 
 
+#import libraries
 #!pip install kaggle
 import kaggle
 
-
-# In[6]:
-
-
-get_ipython().system('kaggle datasets download ankitbansal06/retail-orders -f orders.csv')
+!kaggle datasets download ankitbansal06/retail-orders -f orders.csv
 
 
-# In[7]:
+# In[77]:
 
 
+#extract file from zip file
 import zipfile
-zip_ref = zipfile.ZipFile('orders.csv.zip')
-zip_ref.extractall()
-zip_ref.close()
+zip_ref = zipfile.ZipFile('orders.csv.zip') 
+zip_ref.extractall() # extract file to dir
+zip_ref.close() # close file
 
 
-# In[2]:
+# In[145]:
 
 
+#read data from the file and handle null values
 import pandas as pd
-
-
-# In[3]:
-
-
-df = pd.read_csv('orders.csv',na_values =['Not Available', 'unknown'])
-df.head(20)
+df = pd.read_csv('orders.csv',na_values=['Not Available','unknown'])
 df['Ship Mode'].unique()
 
 
-# In[4]:
+# In[154]:
 
 
-df.rename(columns={'Order Id':'order_id' ,'City':'city', 'Ship Mode':'ship_mode'})
-
-
-# In[5]:
-
-
-df.columns=df.columns.str.lower()
-
-
-# In[6]:
-
-
-df.columns=df.columns.str.replace(' ','_')
-
-
-# In[7]:
-
-
+#rename columns names ..make them lower case and replace space with underscore
+#df.rename(columns={'Order Id':'order_id', 'City':'city'})
+#df.columns=df.columns.str.lower()
+#df.columns=df.columns.str.replace(' ','_')
 df.head(5)
 
 
-# In[8]:
+# In[159]:
 
 
-#derive new columns , disscount , sale_price and profit
-df['disscount']=df['list_price']*df['discount_percent']*.01
-
-
-# In[10]:
-
-
-df['sale_price']=df['list_price']-df['disscount']
-
-
-# In[12]:
-
-
+#derive new columns discount , sale price and profit
+#df['discount']=df['list_price']*df['discount_percent']*.01
+#df['sale_price']= df['list_price']-df['discount']
 df['profit']=df['sale_price']-df['cost_price']
+df
 
 
-# In[16]:
+# In[162]:
 
 
-df.dtypes
-
-
-# In[15]:
-
-
+#convert order date from object data type to datetime
 df['order_date']=pd.to_datetime(df['order_date'],format="%Y-%m-%d")
 
 
-# In[18]:
+# In[167]:
 
 
+#drop cost price list price and discount percent columns
 df.drop(columns=['list_price','cost_price','discount_percent'],inplace=True)
 
 
-# In[20]:
+# In[169]:
 
 
+#load the data into sql server using replace option
 import sqlalchemy as sal
-engine = sal.create_engine('mssql://Amit\SQLEXPRESS01/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
+engine = sal.create_engine('mssql://ANKIT\SQLEXPRESS/master?driver=ODBC+DRIVER+17+FOR+SQL+SERVER')
 conn=engine.connect()
 
 
-# In[22]:
+# In[172]:
 
 
-df.to_sql('df_orders',con=conn, index=False , if_exists = 'append')
-
-
-# In[ ]:
-
+#load the data into sql server using append option
+df.to_sql('df_orders', con=conn , index=False, if_exists = 'append')
 
 
 
